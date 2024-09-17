2e7844003bb10a38c8c2dc73850ad0f6e462067e
10221236 <maksatova674@gmail.com>
2024-09-17 21:01:08 +0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val valOne = readln().toInt()
+    val valTwo = readln().toInt()
+
+    val valSumma = valOne + valTwo
+    val valSub = valOne - valTwo
+    val valMult = valOne * valTwo
+    val valDel = valOne / valTwo
+    println("Сумма равна $valSumma")
+    println("вычитание равно $valSub")
+    println("множение равно $valMult")
+    println("деление равно $valDel")
+}
